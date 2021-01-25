a3530a7c985029dfbeb45efda938177779cb37a1
Wren <wrenparker@gmail.com>
2021-01-25 18:20:36 -0500

    Fixed indices, started adding move logic

--- index.js
+++ index.js
@@ -16,30 +16,107 @@ main = () => {
 
 class PegSolitaire {
     firstMove = true;
+    activeMove = false;
 
     constructor() {
-        this.rowOne = ["A1"]
-        this.rowTwo = ["B1", "B2"]
-        this.rowThree = ["C1", "C2", "C3"]
-        this.rowFour = ["D1", "D2", "D3", "D4"]
-        this.rowFive = ["E1", "E2", "E3", "E4", "E5"]
-        this.board = [this.rowOne, this.rowTwo, this.rowThree, this.rowFour, this.rowFive]
+
+        this.rowZero = [this.buildPeg("A0")];
+        this.rowOne = [this.buildPeg("B0"), this.buildPeg("B1")];
+        this.rowTwo = [this.buildPeg("C0"), this.buildPeg("C1"), this.buildPeg("C2")];
+        this.rowThree = [this.buildPeg("D0"), this.buildPeg("D1"), this.buildPeg("D2"), this.buildPeg("D3")];
+        this.rowFour = [this.buildPeg("E0"), this.buildPeg("E1"), this.buildPeg("E2"), this.buildPeg("E3"), this.buildPeg("E4")];
+        this.board = [this.rowZero, this.rowOne, this.rowTwo, this.rowThree, this.rowFour];
     }
 
-    setUpClicks() {
+    buildPeg = (name)  => {
+        return new Peg(name);
+    }
+
+    setUpClicks = () => {
         this.board.forEach(row => {
-            row.forEach(element => {
-                let piece = document.getElementById(element);
-                piece.addEventListener("click", ()=>{
-                    if(this.firstMove)
-                        piece.innerHTML = "O";
+            row.forEach(piece => {
+                //let piece = document.getElementById(element.name);
+                piece.boardElement.addEventListener("click", ()=>{
+                    if(this.firstMove) {
+                        piece.boardElement.innerHTML = "O";
+                        piece.isActive = false;
                         this.firstMove = false;
+                    }
+
+                    else if(this.isAvailableToMove(piece)) {
+                        let moves = this.findMoves(piece);
+                        console.log(moves);
+                    }
                 })
             })
         })
-        console.log("hello")
+    }
+
+    isAvailableToMove = (element) => {
+        return (!this.firstMove && !this.activeMove && element.isActive)
+    }
+
+    findMoves = (piece) => {
+        let letter = piece.name.slice(0,1);
+        let currentRow = this.findCurrentRow(letter);
+
+        let number = piece.name.slice(1);
+        
+        if(currentRow == this.board[0]) {
+            console.log("Finding moves for A0")
+            return this.findMovesA0();
+        } else
+        if(currentRow == this.board[1]) {
+            return this.findMovesB(number);
+        }
+
+    }
+
+    findMovesB = (number) => {
+        if(number=="0") return this.findMovesB0();
+        if(number=="1") return this.findMovesB1();
+    }
+
+    findMovesB0 = () => {
+        let moves = []
+        if(this.board[2][0].isActive && !this.board[3][0].isActive) moves.push( [this.board[2][0], this.board[3][0]] );
+        if(this.board[2][1].isActive && !this.board[3][2].isActive) moves.push( [this.board[2][1], this.board[3][2]] );
+        return moves;
+    }
+
+    findMovesB1 = () => {
+        let moves = []
+        if(this.board[2][1].isActive && !this.board[3][1].isActive) moves.push( [this.board[2][1], this.board[3][1]] );
+        if(this.board[2][2].isActive && !this.board[3][3].isActive) moves.push( [this.board[2][2], this.board[3][3]] );
+        return moves;
+    }
+
+    findMovesA0 = () => {
+        let moves = []
+        if(this.board[1][0].isActive && !this.board[2][0].isActive) moves.push( [this.board[1][0], this.board[2][0]] );
+        if(this.board[1][1].isActive && !this.board[2][2].isActive) moves.push( [this.board[1][1], this.board[2][2]] );
+        return moves;
+    }
+
+    findCurrentRow = (letter) => {
+        if(letter==="A") return this.rowZero;
+        else if(letter==="B") return this.rowOne;
+        else if(letter==="C") return this.rowTwo;
+        else if(letter==="D") return this.rowThree;
+        else if(letter==="E") return this.rowFour;
+    }
+
+}
+
+class Peg {
+    constructor(name) {
+        this.name = name
+        this.isActive = true;
+        this.boardElement = document.getElementById(name);
+        this.htmlText = this.boardElement.innerHTML
     }
 }
+
 window.onload= () => {
     let start = new PegSolitaire();
     start.setUpClicks();
